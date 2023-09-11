65dfcd10ffcf836932fa9bd07a0378f0801ce511
Rachel Katz <rachel.katz@flatironschool.com>
2023-09-11 16:37:48 -0400

    removed solution

--- README.md
+++ README.md
@@ -1,16 +1,13 @@
-# OOP 2: Class Methods & Class Variables
+# SQL Fundamentals & Table Relations
 
 ## Learning Goals
 
-- Demonstrate Decorators 
-    - `@decorator`
-- Demonstrate class variables
-    - Defining class variables
-    - Updating class variables 
-- Demonstrate class methods
-    - Defining class methods 
-    - `@classmethod`
-    - `cls keyword`
-- Object Inheritance
-- Stretch Goals
-    - Super
+- Explain why we use databases
+- Explain what SQL is and why we use it
+- Explain the differences between a database, server, and API
+- Explain the differences between rows and columns in a table
+- Explain the differences between a Foreign key and a Primary key
+- Explain what a join table is
+- Explain what it means to seed a database
+- Observe using SQL to communicate with a database
+- Explain what an ORM is
---
+++ dog_walker/assets/-- SQLite.sql
@@ -0,0 +1,41 @@
+-- SQLite
+-- ✅ 1. Select specific columns
+
+-- ✅ 2. Create new database structure
+-- ✅ 2a. Remove owners table
+-- ✅ 2b. Create owners table
+-- ✅ 2c. Create pets table
+
+-- ✅ 3. Modify pets table
+-- ✅ 3a. Add image_url to pets
+-- ✅ 3b. Rename column
+
+-- ✅ 4. Create new instances
+-- ✅ 4a. Use chatGPT to create random owners
+-- ^ "given an sql table 'owners' with columns 'name' (text), 'address' (text), 'email' (text), 'phone' (text) create 5 instances with dummy data"
+-- ✅ 4b. Use chat GPT to create sample data for pets
+
+-- ✅ 5. Read data
+-- ✅ 5a. Get all columns from pets
+-- ✅ 5b. Select pet by name
+-- ✅ 5c. Select pets by favorite treats
+-- ✅ 5d. Select pets by age 
+
+-- ✅ 6. Update data
+-- ✅ 6a. Update pet's age by name
+-- ✅ 6b. Update multiple pets' favorite_treats
+
+-- ✅ 7. Delete data
+
+-- ✅ 8. Join data 
+-- ✅ 8a. One to many
+-- ✅ 8b. Many to many: create staff table 
+-- ✅ 8c. Many to many: create appointments table
+
+-- ✅ 9. Seed data
+-- ✅ 9a. Create two staff members using ChatGPT?
+-- ✅ 9b. Create six appointments using ChatGPT
+
+-- ✅ 10. Join tables
+-- ✅ 10a. Basic join
+-- ✅ 10b. Basic join with specific values
