ae997f501405b839bfb32eaeab66703333098fd6
Rachel Katz <rachel.katz@flatironschool.com>
2023-09-11 16:12:33 -0400

    removed solution

---
+++ lib/pet.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+#✅ 1. Create a Pet class
+
+#✅ 2. Instantiate a few pet instances in ipdb 
+#✅ 2a. Compare the pet instances to demonstrate they are not the same object
+
+#✅ 3. Demonstrate __init__ 
+#✅ 3a. Add parameters for attributes (NO OWNER YET, SEE 3D)  
+#✅ 3b. use dot notation to access their attributes 
+#✅ 3c. update attributes with new values 
+#✅ 3d. add owner attribute with default value
+
+#✅ 4. Demonstrate INSTANCE methods
+#✅ 4a. Create a hello method that will print "Hello!"
+#✅ 4b. Create a print_pet_details function that will print the pet attributes
+
+#✅ 5. Set constraints for updating properties (attributes that are controlled by methods)
+#✅ 5d.  Add parameter to pass in name property value on instantiation
+#✅ 5a. Create get/set instance methods for name property 
+#✅ 5b. Create constraint to make sure the name is of type string
+#✅ 5c. Compile get_name, set_name under the same property name
